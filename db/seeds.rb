bf2aa71722665c5dbe82b12bad8c1f16749eea4a
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-12-17 15:51:19 -0700

    migrated tables seeds still need to show on table tho

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 # Example:
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
+#     MovieGenre.find_or_create_by!(name: genre_name)o
 #   end
 
 Poster.create(name: "REGRET",
