001c0253957a7f4a7298f06a0a102003775a3f42
Jackson Bickler <jbickler4@gmail.com>
2025-02-19 11:50:03 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,45 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Poster.create(name: "REGRET",
+    description: "Hard work rarely pays off.",
+    price: 89.00,
+    year: 2018,
+    vintage: true,
+    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+Poster.create(name: "FUTILITY",
+    description: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d",
+    price: 150.00,
+    year: 2016,
+    vintage: false,
+    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+Poster.create(name: "HOPELESSNESS",
+    description: "Stay in your comfort zone; it's safer.",
+    price: 112.00,
+    year: 2020,
+    vintage: true,
+    img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+Poster.create(name: "PROCRASTINATION",
+    description: "Better to avoid failure by not trying at all.",
+    price: 48.00,
+    year: 2017,
+    vintage: true,
+    img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+Poster.create(name: "FEAR",
+    description: "Giving up is always an option.",
+    price: 91.00,
+    year: 2014,
+    vintage: false,
+    img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+Poster.create(name: "DOUBT",
+    description: "Success is for other people, not you.",
+    price: 140.00,
+    year: 2020,
+    vintage: false,
+    img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
