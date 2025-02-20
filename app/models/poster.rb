514d8b372ec754b372cac8851d111188fddcd883
Sebastian McKee <sebmcke@gmail.com>
2025-02-20 14:30:09 -0600

    Add filter by name functionality

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,3 +1,13 @@
 class Poster < ApplicationRecord
-    
+    def self.index(params)
+        queryResult = nil
+        
+        if params["name"]
+            queryResult = Poster.where("name like ?", "%#{params['name'].upcase}%")
+        else
+            queryResult = Poster.all
+        end
+
+        PosterSerializer.new(queryResult)
+    end
 end
