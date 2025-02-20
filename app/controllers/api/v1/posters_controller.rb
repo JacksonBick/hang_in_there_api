8e079c7f3c71a15a262db2619ea2aa03bf04125e
Jackson Bickler <jbickler4@gmail.com>
2025-02-20 09:35:06 -0700

    create method

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -6,4 +6,16 @@ class Api::V1::PostersController < ApplicationController
     def show
         render json: Poster.find(params[:id])
     end
+
+    def create
+  
+        poster = Poster.new(poster_params)
+      
+        if poster.save
+          render json: PosterSerializer.format_single_poster(poster, {})
+        else
+          render json: ErrorSerializer.format_error(422, poster.errors.full_messages.join(", ")), status: :unprocessable_entity
+        end
+    end
+    
 end
