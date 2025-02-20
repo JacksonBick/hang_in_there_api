7e569e0f060d52f3875e2e17ad45c447dafab7a5
Sebastian McKee <sebmcke@gmail.com>
2025-02-20 14:32:27 -0600

    Add min_price filter functionality

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,9 +1,11 @@
 class Poster < ApplicationRecord
     def self.index(params)
         queryResult = nil
-        
+
         if params["name"]
             queryResult = Poster.where("name like ?", "%#{params['name'].upcase}%")
+        elsif params["min_price"]
+            queryResult = Poster.where("price > ?", params["min_price"])
         else
             queryResult = Poster.all
         end
